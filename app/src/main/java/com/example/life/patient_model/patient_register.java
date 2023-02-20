69e622d0d2e47b6772a3501b7473aa894e124d4d
tushar <you@example.com>
2023-02-20 23:23:13 +0530

    login error

---
+++ app/src/main/java/com/example/life/patient_model/patient_register.java
@@ -0,0 +1,14 @@
+package com.example.life.patient_model;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class patient_register extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.patient_register);
+    }
+}
